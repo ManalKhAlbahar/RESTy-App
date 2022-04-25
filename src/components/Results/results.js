5002b35c78f3fe8982fb7e4dd66e9b39e6580c9b
Manal Albahar <manal.albahar@gmail.com>
2022-04-25 23:51:49 +0300

    lab 26

---
+++ src/components/Results/results.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Result(props){
+    return(
+        <section>
+        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+        </section>
+    );
+}
+
+export default Result;
